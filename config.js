3f896154d26ba3c94d141f59dc6d3fce36ef1b7c
Chenzhou16 <86771468+Chenzhou16@users.noreply.github.com>
2021-07-01 22:40:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
     introTitle: 'Ê nhóc con!',
     introDesc: `Thông minh là IQ,
-    
     còn thích em là like you,
-    
     tiếng Anh My là của tôi,
-    
     nên em là người được anh yêu`,
     btnIntro: 'Anh hâm à?',
     title: 'Gọi em là TS, để đi kèm TC',
@@ -15,7 +12,7 @@ const CONFIG = {
     question:'Sao em lại yêu anh nhờ ?',
     btnReply: 'Anh dở hơi!',
     reply: 'Thích thì yêu, không thích thì yêu',
-    mess: '232 tháng có gần 7000 ngày',
+    mess: '233 tháng có gần 7000 ngày',
     messDesc: 'Nhưng với anh ngày đẹp nhất là ngày bên em',
     btnAccept: 'Đớp thính <3',
     messLink: 'http://m.me/chau.ho.583' //link mess của các bạn. VD: https://m.me/nam.nodemy
