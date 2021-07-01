f20a2086bd99288b4c65e9e9a90c203831f20c4e
Chenzhou16 <86771468+Chenzhou16@users.noreply.github.com>
2021-07-01 16:33:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Ê nhóc con',
+    introTitle: 'Ê nhóc con!',
     introDesc: `Thông minh là IQ
-    Còn thích em là like you
-    Tiếng Anh My là của tôi
-    Nên em là người được anh yêu`,
+    <br/> Còn thích em là like you
+    <br/> Tiếng Anh My là của tôi
+    <br/> Nên em là người được anh yêu`,
     btnIntro: 'Anh hâm à?',
-    title: 'Trà đổ vào sữa hay sữa đổ vào trà ?',
-    desc: 'Em đổ anh hay anh đổ em thì cũng như vậy mà',
-    btnYes: 'Không anh chứ ai',
+    title: 'Gọi em là Trà sữa, để đi kèm Tran Chau',
+    desc: 'Em có muốn làm ly trà sữa ngọt ngào ở bên cạnh Tran Chau béo ngậy thơm lừng không ? <3',
+    btnYes: 'Đương nhiên là có',
     btnNo: 'Mơ hả anh trai :3',
-    question:'Em có muốn làm ly trà sữa ngọt ngào ở bên cạnh trân châu béo ngậy thơm lừng không ? <3',
-    btnReply: 'Chơi luôn',
-    reply: 'Đương nhiên là cóooooo',
-    mess: 'Nhưng mà ngày đẹp nhất là ngày bên em',
-    messDesc: '232 tháng có 6965 ngày',
+    question:'Sao em lại yêu anh nhờ ?',
+    btnReply: 'Anh dở hơi!',
+    reply: 'Thích thì yêu thôi',
+    mess: '232 tháng có 6965 ngày',
+    messDesc: 'Nhưng mà ngày đẹp nhất là ngày bên em',
     btnAccept: 'Đớp thính <33',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/chau.ho.583' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
